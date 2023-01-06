63d51e00f3ef2fb1b419abd47e665f3646a7591e
shalash23 <103309490+shalash23@users.noreply.github.com>
2023-01-06 20:59:16 +0200

    will merge

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,5 +1,5 @@
 export async function getCountries() {
   const data = await fetch("https://restcountries.com/v3.1/all");
-  const countries = await reponse.json();
+  const countries = await data.json();
   return countries;
 }
--- src/store/CountriesContext.jsx
+++ src/store/CountriesContext.jsx
@@ -1,22 +1,13 @@
 import React, { createContext, useState } from "react";
-import axios from "axios";
+import { useLoaderData } from "react-router-dom";
 
 export const countriesContext = createContext();
 export const randomCountriesContext = createContext(null);
 
 export const CountriesContextProvider = ({ children }) => {
-  const baseUrl = `https://restcountries.com/v3.1/all`;
-  const [allCountries, setAllCountries] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { countries } = useLoaderData();
+  const [allCountries, setAllCountries] = useState(countries);
 
-  React.useEffect(() => {
-    axios.get(baseUrl).then((response) => {
-      setAllCountries(response.data);
-      setLoading(false);
-    });
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
   const randomCountries = [];
   for (let i = 0; i < 8; i++) {
     let randomNumber = Math.floor(Math.random() * allCountries.length);
